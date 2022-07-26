46b2f34bf7e32151f5dc11733a12a84016ec7a4d
ryuuu1234 <pharyyady@gmail.com>
2022-07-26 10:58:19 +0700

    ini mastering laravel 8 with auth

---
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\v1\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = User::query()->get();
+
+        // return new JsonResponse([
+        //     'data' => $data
+        // ]);
+
+        return UserResource::collection($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/v1/users.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\v1\UserController;
+use Illuminate\Support\Facades\Route;
+
+// Route::middleware('auth:sanctum')
+//     ->group(function () {
+//         Route::get('/me', [UserController::class, 'me']);
+//     });
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/show/{user}', [UserController::class, 'show']);
